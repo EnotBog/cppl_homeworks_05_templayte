7dd61d55ec7533232603d212827e8b562da48644
EnotBog <enotbog@gmail.com>
2023-05-20 16:04:33 +0300

    Edit templ_03

--- templ_03/Source.cpp
+++ templ_03/Source.cpp
@@ -1,28 +1,32 @@
-#include <windows.h>
+﻿#include <windows.h>
 #include <locale.h>
 #include <vector>
 #include <iostream>
+#include <algorithm>
+#include <list>
 
 
-//template<class T,class T2>
+//template<class T>
 
 class SimpleFunctor
 {
 public:
 	SimpleFunctor(): m_counter_dev(0), m_sum_dev3(0){};
-	SimpleFunctor(std::vector<int> a) : m_counter_dev(0), m_sum_dev3(0) {};
+	//SimpleFunctor(std::vector<int> a) : m_counter_dev(0), m_sum_dev3(0) {};
 	~SimpleFunctor() {};
 
 	int get_sum() const {return m_sum_dev3 ; }
 	int get_count() const { return m_counter_dev; }
 
-	void operator ()(std::vector<int> a)
+	void operator ()(int& buf)
 	{
-		for (auto buf : a)
-		{
-			if ((buf % 3) == 0) { ++m_counter_dev; m_sum_dev3 += buf; }
-		}
+		if ((buf % 3) == 0) { ++m_counter_dev; m_sum_dev3 += buf; }
 	}
+
+	//SimpleFunctor operator =()
+	//{
+
+	//}
 
 private:
 	int m_counter_dev;
@@ -37,14 +41,18 @@ int main()
 	setlocale(LC_ALL, "Russian");
 	system("chcp 1251");
  
-	SimpleFunctor sf;
+	SimpleFunctor sf2, sf3 ;
 	std::vector<int> vec = { 4, 1, 3, 6, 25, 54 };
-	//get_sum();
-	sf(vec);
+	std::list<int> vec2 = { 4, 1, 3, 6, 25, 54 };
+	
+	sf2 = std::for_each(vec.begin(), vec.end(), sf2);
+	sf3= std::for_each(vec2.begin(), vec2.end(), sf3);
 	std::cout << "[IN]: 4 1 3 6 25 54\n";
-	std::cout << "[OUT]: get_sum() = "<<sf.get_count()<<"\n";
-	std::cout <<"[OUT]: get_count() = "<< sf.get_sum()<<"\n";
 
+	std::cout << "[OUT]: get_sum() = "<<sf2.get_count()<<"\n";
+	std::cout <<"[OUT]: get_count() = "<< sf2.get_sum()<<"\n";
 
+	std::cout << "[OUT]: get_sum() = " << sf3.get_count() << "\n";
+	std::cout << "[OUT]: get_count() = " << sf3.get_sum() << "\n";
 
 }
